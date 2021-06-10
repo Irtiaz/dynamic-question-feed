fd9d171fa6a8b3dbdb5709aae13321b5a8189afe
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2021-06-10 16:18:02 +0600

    added another email in about page

--- pages/about.js
+++ pages/about.js
@@ -13,12 +13,16 @@ export default class About extends React.Component {
 				<div>
 					<div className={styles.credits}>
 						<b>Credits</b> <br />
-						Question Review and solution : Md Imtiaz Kabir <br />
+						Question review and solution : Md Imtiaz Kabir <br />
 						Developer : Md Irtiaz Kabir
 					</div>
 					<div className={styles.contact}>
-						If you have any interesting question that you want share with us or you have noticed a calculation/concept error or a bug in our site then please feel free to contact us
-						<div className={styles.email}>E-mail: irtiazkabir.irtiaz@gmail.com</div>
+						If you have any interesting question that you want share with us or you have noticed a calculation/concept error or a bug in our site then please feel free to contact us <br /> <br /> <br />
+						E-mail :
+						<div className={styles.email}>
+							imtiazkabir.imtiaz@gmail.com<br />
+							irtiazkabir.irtiaz@gmail.com
+						</div>
 					</div>
 					And here's{" "} 
 					<a href="https://github.com/Irtiaz/dynamic-question-feed">the source code for this project</a> <br />
