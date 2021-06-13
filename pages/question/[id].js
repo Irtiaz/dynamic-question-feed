a9d2f33eaff77b6a917321d611116a8691053f88
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2021-06-13 18:33:11 +0600

    Fixed fallback

--- pages/question/[id].js
+++ pages/question/[id].js
@@ -1,48 +1,56 @@
-import React from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
 import mongoose from 'mongoose';
+import { useRouter } from 'next/router';
 
 import QuestionModel from '../../models/QuestionModel.js';
 import Question from '../Components/Question.js';
 
 import styles from '../../styles/SpecificQuestion.module.css';
 
-export default class SpecificQuestion extends React.Component {
-	render() {
-		const question = JSON.parse(this.props.question);
+export default function SpecificQuestion(props) {
+	const question = JSON.parse(props.question);
 
+	const router = useRouter();
+
+	let Content;
+	if (router.isFallback) {
+		Content = <div className={styles.loading}>Loading ...</div>
+	} else {
 		const {_id, ques, quesImageBase64, quesImageWidth, quesImageHeight, ans, ansImageBase64, ansImageWidth, ansImageHeight, keywords} = question;
-
-		return (
-			<>
-				<Head>
-					<title>A question from BRAIN FREEZE</title>
-				</Head>
-				<div>
-					<Question 
-						id={_id}
-						ques={ques}
-						quesImageBase64={quesImageBase64}
-						quesImageWidth={quesImageWidth}
-						quesImageHeight={quesImageHeight}
-						ans={ans}
-						ansImageBase64={ansImageBase64 || ""}
-						ansImageWidth={ansImageWidth}
-						ansImageHeight={ansImageHeight}
-						keywords={keywords}
-					/>
-					<div className={styles.link_container}>
-						Visit{" "}
-						<Link href="/">
-							<a>BRAINFREEZE</a>
-						</Link>{" "}
-						for more questins like this
-					</div>
+		Content = 
+			<div>
+				<Question 
+					id={_id}
+					ques={ques}
+					quesImageBase64={quesImageBase64}
+					quesImageWidth={quesImageWidth}
+					quesImageHeight={quesImageHeight}
+					ans={ans}
+					ansImageBase64={ansImageBase64 || ""}
+					ansImageWidth={ansImageWidth}
+					ansImageHeight={ansImageHeight}
+					keywords={keywords}
+				/>
+				<div className={styles.link_container}>
+					Visit{" "}
+					<Link href="/">
+						<a>BRAINFREEZE</a>
+					</Link>{" "}
+					for more questins like this
 				</div>
-			</>
-		);
+			</div>
 	}
+
+
+	return (
+		<>
+			<Head>
+				<title>A question from BRAIN FREEZE</title>
+			</Head>
+			{Content}
+		</>
+	);
 }
 
 
@@ -92,6 +100,6 @@ export async function getStaticPaths(context) {
 
 	return {
 		paths,
-		fallback: false
+		fallback: true
 	};
 }
