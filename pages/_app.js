2e3aa2cfe963bbaf4fab6bcf70e72e22e5bc8397
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2021-06-16 21:11:22 +0600

    removed comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,11 @@
 import Navbar from './Components/Navbar.js';
 import Container from './Components/Container.js';
 
-//import { useEffect } from 'react';
 import 'katex/dist/katex.min.css';
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-
-	//useEffect(async () => {
-		//(await import('katex/dist/katex.min.css'));
-		//await require('katex/dist/katex.min.css');
-	//});
 
 	return (
 		<>
