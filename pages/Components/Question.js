32b7e90e9f3ad722b518e847a322cea151e782de
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2021-06-16 18:37:22 +0600

    Support for basic markdown enabled

--- pages/Components/Question.js
+++ pages/Components/Question.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import MathJax from 'react-mathjax';
+import ReactMarkdown from 'react-markdown';
 
 import ContentCopyIcon from 'mdi-react/ContentCopyIcon';
 import ReloadIcon from 'mdi-react/ReloadIcon';
@@ -150,6 +151,7 @@ export default class Question extends React.Component {
 					</div>
 					{Solution}
 				</MathJax.Provider>
+
 			</div>
 		);
 	}
@@ -170,7 +172,7 @@ function getLines(text){
 		for (let token of line)	{
 			if (token[0] === "`") ln.push(<MathJax.Node key={shortid.generate()} inline formula={token.substr(1, token.length - 2)}/>);
 			else if (token[0] === "@") ln.push(<MathJax.Node key={shortid.generate()} formula={token.substr(1, token.length - 2)} />);
-			else ln.push(<span key={shortid.generate()}>{token}</span>);
+			else ln.push(<ReactMarkdown key={shortid.generate()}>{token}</ReactMarkdown>)			
 		}
 		lines.push(<div key={shortid.generate()}>{ln}</div>);
 	}
